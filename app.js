4f8dc74537016e95ff3a206f90c5d4675d1476b9
Łukasz <zahuta@gmail.com>
2016-07-26 15:58:28 +0200

    added monster creator

--- app.js
+++ app.js
@@ -9,9 +9,34 @@ function createChar() {
 	var hero = new Actor();
 	hero.addName(name);
 	console.log("Hero has a name", hero);
+	return hero;
 }
+
+var monster = new Monster();
+console.log("Monster is: ", monster);
 
 function randomizer(min,max) {
 	var number = Math.floor(Math.random() * max + min);
-	console.log(number);
+}
+
+function fight(hero, enemy) {
+	var heroAtt = hero["str"] + hero["dex"];
+	var heroRand = randomizer(1, 10);
+	var monAtt = enemy["str"] + enemy["dex"];
+	var monRand = randomizer(1, 10);
+
+	console.log(heroRand, monRand, heroAtt, monAtt);
+	console.log("Your attack: " + heroAtt + " monster attack: " + monAtt);
+
+	if (heroAtt > monAtt) {
+		console.log("You hit the monstah!");
+		hero.exp = hero.exp + enemy.exp;
+	} else if (monAtt < heroAtt) {
+		console.log("OUCH! You got hit!");
+	} else {
+		console.log("A parry!");
+	}
+
+	console.log(hero["exp"]);
+
 }
---
+++ monster.js
@@ -0,0 +1,9 @@
+function Monster(name, str, dex, sta, int, wis, exp) {
+	this.name = name || "Unnamed";
+	this.str = str || 1;
+	this.dex = dex || 1;
+	this.sta = sta || 1;
+	this.int = int || 1;
+	this.wis = wis || 1;
+	this.exp = exp || 1;
+}
