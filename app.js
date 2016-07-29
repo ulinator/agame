959c2878c863882dcb6fdba76e5510d8ceb40be4
Łukasz <zahuta@gmail.com>
2016-07-29 13:41:02 +0200

    fight mechanics update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // BUTTONS
 var button = document.querySelector("#createChar");
+var game = new Game("inactive");
 var hero;
 var timer = new Timer("paused", gameAction);
 
@@ -9,12 +10,12 @@ button.addEventListener("click", function() {
 
 var buttonStart = document.querySelector("#startGame");
 buttonStart.addEventListener("click", function() {
-	timer.tick(2000);
+	game.changeState("search");
 });
 
 var buttonPause = document.querySelector("#pauseGame");
 buttonPause.addEventListener("click", function() {
-	timer.tickStop();
+	game.changeState("inactive");
 });
 
 // CREATE MONSTER
@@ -37,79 +38,10 @@ function gameAction() {
 
 	if (randomExpression < 99) {
 		console.log("It's a fight!");
-		fight(hero,monster);
+		game.fight(hero,monster);
 	} else if (randomExpression < 100) {
 		console.log("It's a KISS!");
 	} else {
 		console.log("Smieszki he he");
 	}
 };
-
-
-// FIGHT MECHANICS
-
-function clashWinner(attacker,defender) {
-	var val1 = attacker.getAttack();
-	var val2 = defender.getAttack();
-
-	if ( val1 > val2 ) {
-		return attacker;
-	} else if (val1 < val2 ) {
-		return defender;
-	} else {
-		return null;
-	}
-};
-
-function clash(attacker, defender) {
-	var winner = clashWinner(attacker,defender);
-	var heroHP = hero.getHP();
-	var monHP = monster.getHP();
-	var heroAtt = hero.getAttack();
-	var monAtt = monster.getAttack();
-	
-	if (winner === attacker) {
-		console.log("hero won");
-		monHP = monHP - heroAtt;
-		console.log("monster hp is down to: ", monHP);
-		return monHP;
-	} else if (winner === defender)  {
-		console.log("monster won");
-		heroHP = heroHP - monAtt;
-		console.log("hero hp is down to: ", heroHP);
-		return heroHP;
-	} else {
-		console.log("parry");
-		return null;
-	}
-};
-
-function fight(attacker, defender) {
-	// var ticker = new Timer("paused", clash);
-	// timer.tickStop();
-
-	var heroHP = hero.getHP();
-	var heroAtt = hero.getAttack();
-	console.log("hero HP ", heroHP + "hero Att: ", heroAtt);
-
-	var monHP = monster.getHP();
-	var monAtt = monster.getAttack();
-	console.log("monster HP ", monHP + "monster Att: ", monAtt);
-
-	if (heroHP > 0 && monHP > 0 ) {
-		clash(attacker, defender);
-	} else if ( heroHP <= 0) {
-		console.log("You died.");
-		timer.tickStop();
-	} else if ( monsterHP <= 0) {
-		console.log("You killed the " + defender.name + "!");
-	}
-	// while ( (heroHP > 0) || (monHP > 0) ) {
-	// 	setTimeout(function(){ 
-	// 		alert("fight round"); 
-	// 		clash(hero, monster);
-	// 	}, 1000);
-	// };
-
-};
-
--- game.js
+++ game.js
@@ -1,3 +1,75 @@
 function Game(state) {
-	this.state = state;
+	this.state = state || "inactive";
 }
+
+Game.prototype.changeState = function(param) {
+	this.state = param;
+	if ( (param === "search") || (param === "fight") ) {
+		timer.tick(2000);
+	} else {
+		timer.tickStop();
+	}
+};
+
+Game.prototype.fight = function(attacker, defender) {
+	game.state = "fight";
+
+	var heroHP = hero.getHP();
+	var heroAtt = hero.getAttack();
+	console.log("hero HP ", heroHP + "hero Att: ", heroAtt);
+
+	var monHP = monster.getHP();
+	var monAtt = monster.getAttack();
+	console.log("monster HP ", monHP + "monster Att: ", monAtt);
+
+	var resolveRound = function(attacker,defender) {
+		var val1 = attacker.getAttack();
+		var val2 = defender.getAttack();
+
+		if ( val1 > val2 ) {
+			return attacker;
+		} else if (val1 < val2 ) {
+			return defender;
+		} else {
+			return null;
+		}
+	};
+
+	var round = function(attacker, defender) {
+		var result = resolveRound(attacker,defender);
+
+		if (result === attacker) {
+			console.log("hero won");
+			monHP = monHP - heroAtt;
+
+			if (monHP <= 0) {
+				console.log("You killed the " + defender.name + "!");
+				attacker.exp = attacker.exp + defender.exp;
+				game.state = "search";
+				return null;
+			} else {
+				console.log("monster hp is down to: ", monHP);
+				return monHP; 
+			};
+		} else if (result === defender)  {
+			console.log("monster won");
+			heroHP = heroHP - monAtt;
+
+			if (heroHP <= 0) {
+				console.log("You were mortally hit by" + defender.name + ".");
+				game.state = "inactive";
+				console.log("You died.");
+				return null;
+			} else {
+				console.log("Your hp is down to: ", heroHP);
+				return heroHP; 
+			};
+		} else {
+			console.log("parry");
+			return null;
+		}
+	};
+
+	round(attacker,defender);
+
+};
